7891edb7168fe2df58d86f97afdcf57bffb95e2a
Nischal Shakya <nischalshakya2015@gmail.com>
2022-06-14 11:54:26 +0545

    Add hero section

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="flex items-center justify-between bg-yellow-400 border-y-black py-10 lg:py-0">
+      <div className="px-8 space-y-5">
+        <h1 className="text-6xl max-w-xl font-serif">
+          <span className="underline decoration-black decoration-4">
+            Medium
+          </span>{' '}
+          is a place to write, read, and connect
+        </h1>
+        <h2>
+          It&apos;s easy and free to post your thinking on any topic and connect
+          with millions of readers.
+        </h2>
+      </div>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        className="hidden md:inline-flex h-32 lg:h-full"
+        src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+        alt="Medium Logo"
+      />
+    </div>
+  );
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { Navbar } from '../components';
+import { Hero } from '../components';
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Navbar />
+      <Hero />
     </div>
   );
 };
