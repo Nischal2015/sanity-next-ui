48ec674e3b0cf57da7411b6caf4e17957adabf16
Nischal Shakya <nischalshakya2015@gmail.com>
2022-06-14 12:04:08 +0545

    Fix border not showing

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className="flex items-center justify-between bg-yellow-400 border-y-black py-10 lg:py-0">
+    <div className="flex items-center justify-between bg-yellow-400 border border-y-black py-10 lg:py-0">
       <div className="px-8 space-y-5">
         <h1 className="text-6xl max-w-xl font-serif">
           <span className="underline decoration-black decoration-4">
