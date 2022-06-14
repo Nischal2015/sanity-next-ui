fabcec41201e8ef01273e845b977abff9906612f
Nischal Shakya <nischalshakya2015@gmail.com>
2022-06-14 19:01:55 +0545

    Add graphql client setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const client = new ApolloClient({
+    uri: 'https://mog7c77w.api.sanity.io/v1/graphql/production/default',
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
